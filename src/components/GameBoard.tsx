08633ed9a382a6847c054f845c43f9b20e148126
Gregor <43413852+Albadylic@users.noreply.github.com>
2025-01-23 21:01:34 +0000

    Move game logic back to game board

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -22,10 +22,6 @@ interface cardShape {
 
 interface GameBoardProps {
   setGameStarted: React.Dispatch<React.SetStateAction<boolean>>;
-  setPlayerDeck: React.Dispatch<React.SetStateAction<[] | cardShape[]>>;
-  playerDeck: [] | cardShape[];
-  setOpponentDeck: React.Dispatch<React.SetStateAction<[] | cardShape[]>>;
-  opponentDeck: [] | cardShape[];
 }
 
 /**
@@ -36,42 +32,9 @@ interface playerChoiceType {
   playerChoiceValue: number | null;
 }
 
-function reducer(state: any, action: any) {
-  switch (action.type) {
-    case "PLAYER_WIN":
-      return {
-        ...state,
-        playerDeck: [...state.playerDeck, state.opponentDeck[0]],
-      };
-    case "OPPONENT_WIN":
-      return {
-        ...state,
-        opponentDeck: [...state.opponentDeck, state.playerDeck[0]],
-      };
-    case "SET_NEXT_POKEMON":
-      return {
-        ...state,
-        playerDeck: state.playerDeck.slice(1),
-        opponentDeck: state.opponentDeck.slice(1),
-      };
-    default:
-      return state;
-  }
-}
+//
 
-const GameBoard: FC<GameBoardProps> = ({
-  setGameStarted,
-  setPlayerDeck,
-  playerDeck,
-  setOpponentDeck,
-  opponentDeck,
-}) => {
-  const initialState = {
-    playerDeck: playerDeck, // Initial player deck
-    opponentDeck: opponentDeck, // Initial opponent deck
-  };
-
-  const [state, dispatch] = useReducer(reducer, initialState);
+const GameBoard: FC<GameBoardProps> = ({ setGameStarted }) => {
   const [playerChoice, setPlayerChoice] = useState<playerChoiceType | null>(
     null
   );
@@ -80,39 +43,73 @@ const GameBoard: FC<GameBoardProps> = ({
   const [turnOutcome, setTurnOutcome] = useState<string | null>(null);
   const [score, setScore] = useState<number>(0);
 
-  useEffect(() => {
-    if (playerDeck.length > 0 && opponentDeck.length > 0) {
-      dispatch({ type: "SET_NEXT_POKEMON" });
-    } else if (playerDeck.length === 0) {
-      setGameOutcome("lose");
-    } else if (opponentDeck.length === 0) {
-      setGameOutcome("win");
-    }
-  }, [playerDeck, opponentDeck]);
+  const [playerDeck, setPlayerDeck] = useState<cardShape[] | []>([]);
+  const [opponentDeck, setOpponentDeck] = useState<cardShape[] | []>([]);
+
+  // Default to 10 cards each
+  // Fetch 20 cards
+  const [deckSize, setDeckSize] = useState<number>(20);
+
+  function randomID() {
+    return Math.floor(Math.random() * 151) + 1;
+  }
 
   useEffect(() => {
-    console.log(state.opponentDeck[0]);
+    async function getPokemon() {
+      const ids: number[] = [];
+      const url: string = `https://pokeapi.co/api/v2/pokemon/`;
+
+      while (ids.length < deckSize) {
+        let id = randomID();
+        if (ids.indexOf(id) === -1) {
+          ids.push(id);
+        }
+      }
+
+      const promises = ids.map((id) =>
+        fetch(`${url}${id}`).then((response) => response.json())
+      );
+
+      try {
+        const results = await Promise.all(promises);
+
+        // Split cards between player and opponent
+        const playerCards: cardShape[] = [];
+        const opponentCards: cardShape[] = [];
+
+        results.forEach((item, index) =>
+          index % 2 === 0 ? playerCards.push(item) : opponentCards.push(item)
+        );
+
+        setPlayerDeck(playerCards);
+        setOpponentDeck(opponentCards);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    getPokemon();
+  }, [deckSize]);
+
+  useEffect(() => {
     const compareValues = () => {
       const playerStat = playerChoice?.playerChoiceName;
       const playerValue = playerChoice?.playerChoiceValue;
 
-      let opponentValue = state.opponentDeck[0].stats.filter((obj: any) => {
+      let opponentValue = opponentDeck[0].stats.filter((obj: any) => {
         return obj.stat.name === playerStat;
       })[0]["base_stat"];
 
       if (playerValue !== null && playerValue !== undefined) {
         if (playerValue >= opponentValue) {
           setTurnOutcome(
-            `You win, you receive opponent's ${state.opponentDeck[0].name}!`
+            `You win, you receive opponent's ${opponentDeck[0].name}!`
           );
           // Pass the opponentCard to the end of playerDeck
-          dispatch({ type: "PLAYER_WIN" });
         } else {
           setTurnOutcome(
-            `You lose, opponent receives your ${state.playerDeck[0].name}!`
+            `You lose, opponent receives your ${playerDeck[0].name}!`
           );
           // Pass the playerCard to the end of opponentDeck
-          dispatch({ type: "OPPONENT_WIN" });
         }
       }
 
@@ -122,23 +119,22 @@ const GameBoard: FC<GameBoardProps> = ({
     if (playerChoice) {
       compareValues();
       // Set next pokemon - should happen when the decks are changed above
-      dispatch({ type: "SET_NEXT_POKEMON" });
     }
-  }, [playerChoice, state.opponentDeck, state.playerDeck]);
+  }, [playerChoice, opponentDeck, playerDeck]);
 
   return (
     <>
       <section className="GameBoard">
-        {state.playerDeck[0] && (
+        {playerDeck[0] && (
           <PlayerCard
-            playerPokemon={state.playerDeck[0]}
+            playerPokemon={playerDeck[0]}
             setPlayerChoice={setPlayerChoice}
           />
         )}
 
-        {state.opponentDeck[0] && (
+        {opponentDeck[0] && (
           <OpponentCard
-            opponentPokemon={state.opponentDeck[0]}
+            opponentPokemon={opponentDeck[0]}
             playerChoice={playerChoice}
           />
         )}
