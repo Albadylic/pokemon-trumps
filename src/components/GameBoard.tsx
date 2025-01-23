c5df26e37d453b735fd689de894d03f686e97c0e
Gregor <43413852+Albadylic@users.noreply.github.com>
2025-01-23 21:34:36 +0000

    Dunno

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -45,6 +45,8 @@ const GameBoard: FC<GameBoardProps> = ({ setGameStarted }) => {
 
   const [playerDeck, setPlayerDeck] = useState<cardShape[] | []>([]);
   const [opponentDeck, setOpponentDeck] = useState<cardShape[] | []>([]);
+  const [currentPlayerCard, setCurrentPlayerCard] = useState<cardShape>();
+  const [currentOpponentCard, setCurrentOpponentCard] = useState<cardShape>();
 
   // Default to 10 cards each
   // Fetch 20 cards
@@ -53,6 +55,20 @@ const GameBoard: FC<GameBoardProps> = ({ setGameStarted }) => {
   function randomID() {
     return Math.floor(Math.random() * 151) + 1;
   }
+
+  const setCurrentCards = () => {
+    if (playerDeck.length === 0) {
+      setGameOutcome("lose");
+    } else if (opponentDeck.length === 0) {
+      setGameOutcome("win");
+    } else {
+      const playerCard = playerDeck[0];
+      const opponentCard = opponentDeck[0];
+
+      setCurrentPlayerCard(playerCard);
+      setCurrentOpponentCard(opponentCard);
+    }
+  };
 
   useEffect(() => {
     async function getPokemon() {
@@ -83,6 +99,8 @@ const GameBoard: FC<GameBoardProps> = ({ setGameStarted }) => {
 
         setPlayerDeck(playerCards);
         setOpponentDeck(opponentCards);
+
+        setCurrentCards();
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -90,51 +108,72 @@ const GameBoard: FC<GameBoardProps> = ({ setGameStarted }) => {
     getPokemon();
   }, [deckSize]);
 
-  useEffect(() => {
-    const compareValues = () => {
-      const playerStat = playerChoice?.playerChoiceName;
-      const playerValue = playerChoice?.playerChoiceValue;
+  const compareValues = () => {
+    const playerStat = playerChoice?.playerChoiceName;
+    const playerValue = playerChoice?.playerChoiceValue;
 
-      let opponentValue = opponentDeck[0].stats.filter((obj: any) => {
-        return obj.stat.name === playerStat;
-      })[0]["base_stat"];
+    let opponentValue = currentOpponentCard?.stats.filter((obj: any) => {
+      return obj.stat.name === playerStat;
+    })[0]["base_stat"];
 
-      if (playerValue !== null && playerValue !== undefined) {
-        if (playerValue >= opponentValue) {
-          setTurnOutcome(
-            `You win, you receive opponent's ${opponentDeck[0].name}!`
-          );
-          // Pass the opponentCard to the end of playerDeck
-        } else {
-          setTurnOutcome(
-            `You lose, opponent receives your ${playerDeck[0].name}!`
-          );
-          // Pass the playerCard to the end of opponentDeck
-        }
+    if (
+      playerValue !== null &&
+      playerValue !== undefined &&
+      opponentValue !== null &&
+      opponentValue !== undefined &&
+      currentPlayerCard !== undefined &&
+      currentOpponentCard !== undefined
+    ) {
+      if (playerValue >= opponentValue) {
+        handleWin("player", currentPlayerCard, currentOpponentCard);
+      } else {
+        handleWin("opponent", currentPlayerCard, currentOpponentCard);
       }
-
-      return null;
-    };
-
-    if (playerChoice) {
-      compareValues();
-      // Set next pokemon - should happen when the decks are changed above
     }
-  }, [playerChoice, opponentDeck, playerDeck]);
+
+    return null;
+  };
+
+  const handleWin = (
+    winner: string,
+    playerCard: cardShape,
+    opponentCard: cardShape
+  ) => {
+    if (winner === "player") {
+      setTurnOutcome(
+        `You win, you receive opponent's ${currentOpponentCard?.name}!`
+      );
+      // Pass the opponentCard to the end of playerDeck
+      setPlayerDeck([...playerDeck.slice(1), playerCard, opponentCard]);
+      setOpponentDeck(opponentDeck.slice(1));
+    } else {
+      setTurnOutcome(
+        `You lose, opponent receives your ${currentPlayerCard?.name}!`
+      );
+      // Pass the playerCard to the end of opponentDeck
+      setOpponentDeck([...opponentDeck.slice(1), opponentCard, playerCard]);
+      setPlayerDeck(playerDeck.slice(1));
+    }
+  };
+
+  if (playerChoice) {
+    compareValues();
+    // Set next pokemon - should happen when the decks are changed above
+  }
 
   return (
     <>
       <section className="GameBoard">
-        {playerDeck[0] && (
+        {currentPlayerCard && (
           <PlayerCard
-            playerPokemon={playerDeck[0]}
+            playerPokemon={currentPlayerCard}
             setPlayerChoice={setPlayerChoice}
           />
         )}
 
-        {opponentDeck[0] && (
+        {currentOpponentCard && (
           <OpponentCard
-            opponentPokemon={opponentDeck[0]}
+            opponentPokemon={currentOpponentCard}
             playerChoice={playerChoice}
           />
         )}
